6e3091ad92ed149ca5f7e31bbc21422868770e16
Smile:] <cryptd.smile@gmail.com>
2026-08-04 16:44:21 -0500

    Footprint trail spacing fixed
    Co-authored-by: JiwonPark <wldnjs95@users.noreply.github.com>

--- scripts/footprints.js
+++ scripts/footprints.js
@@ -4,20 +4,34 @@
 console.log("footprints.js loaded");
 const shoe_cursor = document.querySelector("#cursor");
 
-
-
-// const , let
-
 // 마지막 마우스 x, y를 저장할 변수와, 마지막 발자국 x, y 좌표 변수 만들기
 let lastX = null; // mouse x 좌표
 let lastY = null; // mouse y
 let lastShoeX = null; // shoe x
 let lastShoeY = null; // shoe y
 
+// "어느정도거리" 움직였을때 찍고싶은데?
+const STEP = 30;
+
 document.addEventListener("mousemove", (event) => {
 
     const x = event.clientX;
     const y = event.clientY;
+
+
+    //lastX가 null 이었냐?
+    if (lastX == null) {
+        lastX = x
+        lastY = y
+        return;
+    };
+
+    
+    let dx = x - lastX;
+    let dy = y - lastY;
+    let dist = Math.sqrt(dx * dx + dy * dy);
+
+    if (dist < STEP) return;
 
     // Creates and adds the shoe into the page.
     const shoe = document.createElement("img");
@@ -25,16 +39,24 @@ document.addEventListener("mousemove", (event) => {
     shoe.classList.add("footprint")
     shoe.style.top = y + "px";
     shoe.style.left = x + "px";    
-    document.body.appendChild(shoe);
-
-
-    // 발자국이 마우스가 어느정도 움직였을때만 발자국이 찍히는거만
-    // Math sqrt
-
+    document.body.appendChild(shoe);     
+    
     // 찍은 발자국 삭제
     setTimeout(()=> {shoe.remove()}, 4000);
 
-
-    // 마우스마지막 위치를 저장해둔다.
+    lastX = x;
+    lastY = y;
 
 });
+
+// github 
+
+
+// isLeftFoot True/False, 1,0 변수 활용 1
+// image - Scale (이미지를 좌우 반전 시켜주는 함수, 스타일)
+
+// next steps
+// 발자국 방향(각도) - 삼각함수 sin cos tan, 두점사이의 방향(벡터) atan2 
+// 발자국 애니메이션
+// 발자국 색상 변경,
+// 발자국 양발 사인 간격 
